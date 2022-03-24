8f4c4c8054dccde9477677dbb57013ec6b696bfa
Thitut Priyo Pamungkas <63655121+thitutpp@users.noreply.github.com>
2022-03-24 17:15:54 +0700

    update layout

--- resources/views/layouts/layouts-dashboard.blade.php
+++ resources/views/layouts/layouts-dashboard.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{url('companies')}}">Companies</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{url('employees')}}">Employees</a>
+                          </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
